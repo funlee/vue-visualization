f8b5a79fbc84e4e6f975972c7ef40f26681839db
funlee <i@funlee.cn>
2017-12-29 22:17:16 +0800

    drop down

---
+++ src/components/common/DropDown.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="drop-down">
+    <div class="target-value" :class="{fold: isFold}" @click="showFold">昆明市</div>
+    <ul class="down-list">
+      <li>测试数据</li>
+      <li>测试数据</li>
+      <li>测试数据</li>
+      <li>测试数据</li>
+      <li>测试数据</li>
+      <li>测试数据</li>
+      <li>测试数据</li>
+      <li>测试数据</li>
+      <li>测试数据</li>
+      <li>测试数据</li>
+    </ul>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'dropDown',
+    data () {
+      return {
+        dataset: [],
+        isFold: true
+      }
+    },
+    mounted () {
+      console.log(123)
+    },
+    methods: {
+      showFold () {
+        this.isFold = !this.isFold
+      }
+    }
+  }
+</script>
+<style>
+.drop-down{
+  display: inline-block;
+  position: relative;
+  border: 2px solid rgb(20, 120, 230);
+  border-radius: 4px;
+  background-image: linear-gradient(90deg, rgb(7, 28, 154) 0%, rgb(7, 28, 154) 100%);
+  box-sizing: border-box;
+  cursor: pointer;
+  vertical-align: top;
+}
+.target-value{
+  display: inline-block;
+  font-size:40px;
+  color: #fff;
+  line-height: 70px;
+  position: relative;
+  padding: 0 80px 0 20px;
+  overflow: hidden;
+}
+.target-value:after{
+    content: "";
+    display: inline-block;
+    width: 64px;
+    height: 57px;
+    background: url(../../assets/images/common/fold-down-icon.png) no-repeat;
+    cursor: pointer;
+    position: absolute;
+    top: 50%;
+    right: 0px;
+    transition: all 0.5s;
+    transform: translateY(-50%) rotate(180deg);
+}
+.fold:after{
+  transform: translateY(-50%) rotate(0);
+}
+.down-list{
+    border-top: 2px solid #000108;
+    position: relative;
+}
+.down-list li{
+  cursor: pointer;
+  font-size: 34px;
+  color: #adb8fc;
+  line-height: 40px;
+  white-space: nowrap;
+  user-select: none;
+  margin: 20px 0;
+  text-indent: 20px;
+}
+.down-list li:hover{
+  color: #fff;
+}
+
+</style>
+
--- src/components/stride/fireworks/Fireworks.vue
+++ src/components/stride/fireworks/Fireworks.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="fireworks">
     <TopSide :title="title"></TopSide>
+    <div class="select-wrap">
+      <DropDown></DropDown>
+    </div>
   </div>
 </template>
 
 <script>
 import TopSide from '../../common/TopSide'
+import DropDown from '../../common/DropDown'
 export default {
   data () {
     return {
@@ -13,10 +17,18 @@ export default {
     }
   },
   components: {
-    TopSide
+    TopSide,
+    DropDown
   }
 }
 </script>
 <style scoped>
-
+.select-wrap{
+    position: absolute;
+    top: 200px;
+    left: 70px;
+    height: 72px;
+    padding-bottom: 20px;
+    border-bottom: 2px solid #3a82f8;
+}
 </style>
